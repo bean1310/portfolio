e54c98e1e11f798f1ea6d649239c79492a03c78b
bean1310 <bean@beanlog.xyz>
2022-09-24 15:17:41 +0900

    Delete v1 files

--- src/views/Home.vue
+++
@@ -1,8 +0,0 @@
-<template>
-  <div class="home">
-    <h1>Home</h1>
-    <h2>Welcome to my portfolio</h2>
-    <img src='img/top.jpeg' width="50%">
-    <p>白と黒が好きです．コンピュータの0と1と同じように．</p>
-  </div>
-</template>
