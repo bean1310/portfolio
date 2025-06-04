f5592cbe37e4c00861d83f3fbe2ff5aee0fdcb96
MATSUSHITA Isato <bean@beanlog.xyz>
2025-06-05 00:46:16 +0900

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { defineConfig } from 'vite';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,7 +7,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
-        additionalData: `@import "./src/assets/prepends.scss";`
+        additionalData: `@use "./src/assets/prepends.scss";`
       }
     }
   }
